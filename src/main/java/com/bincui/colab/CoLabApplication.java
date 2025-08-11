72b7737587953848fe87d3a37540feaafebd6aed
bincui <tnqls3718@gmail.com>
2025-08-11 17:05:44 +0900

    최초 커밋

---
+++ src/main/java/com/bincui/colab/CoLabApplication.java
@@ -0,0 +1,13 @@
+package com.bincui.colab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bincui/colab/CoLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bincui.colab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
